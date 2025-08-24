a1dd4ff1a778de59ec4329b502e98752c119a0b7
jonas-amilton <jonas96@edu.unisinos.br>
2025-08-24 00:14:09 -0300

    feat: tradução

---
+++ config/filament.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filament Language
+    |--------------------------------------------------------------------------
+    |
+    | Define o idioma padrão do painel do Filament.
+    |
+    */
+
+    'language' => 'pt_BR',
+
+    // outras configurações do Filament...
+
+];
