88957c99313b5367f6e94dfb2762ea8b438de284
jonas-amilton <jonas96@edu.unisinos.br>
2025-10-05 17:35:24 -0300

    feat: criado novos endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Livewire\AppointmentBooking;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\{
+    PatientLogin,
+    PatientRegister,
+    PatientDashboard,
+    PatientAppointment
+};
 
 Route::get('/', function () {
-    return redirect('admin');
+    return redirect('agendar');
+});
+
+Route::get('/agendar', AppointmentBooking::class)->name('appointment.booking');
+
+
+Route::get('/login', PatientLogin::class)->name('patient.login');
+Route::get('/register', PatientRegister::class)->name('patient.register');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', PatientDashboard::class)->name('patient.dashboard');
+    Route::get('/agendar', PatientAppointment::class)->name('patient.appointment');
 });
