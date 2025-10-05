ec6d7fd812253296499bd423076637d74645d03a
jonas-amilton <jonas96@edu.unisinos.br>
2025-10-05 18:56:12 -0300

    refactor: rotas simplificadas

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/agendar', AppointmentBooking::class)->name('appointment.booking');
 Route::get('/login', PatientLogin::class)->name('patient.login');
 Route::get('/register', PatientRegister::class)->name('patient.register');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth.patient')->group(function () {
     Route::get('/dashboard', PatientDashboard::class)->name('patient.dashboard');
     Route::get('/agendar', PatientAppointment::class)->name('patient.appointment');
 });
