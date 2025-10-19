540d45b25c14456b42c171e008f044a23865eceb
jonas-amilton <jonas96@edu.unisinos.br>
2025-10-19 17:56:04 -0300

    refactor: relacionamentos

--- app/Models/Specialty.php
+++ app/Models/Specialty.php
@@ -13,28 +13,33 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * Class Specialty
- * 
+ *
  * @property int $id
  * @property string $name
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * 
+ *
  * @property Collection|ProfessionalSpecialty[] $professional_specialties
  *
  * @package App\Models
  */
 class Specialty extends Model
 {
-	use HasFactory;
+    use HasFactory;
 
-	protected $table = 'specialties';
+    protected $table = 'specialties';
 
-	protected $fillable = [
-		'name'
-	];
+    protected $fillable = [
+        'name'
+    ];
 
-	public function professional_specialties()
-	{
-		return $this->hasMany(ProfessionalSpecialty::class);
-	}
+    public function professional_specialties()
+    {
+        return $this->hasMany(ProfessionalSpecialty::class);
+    }
+
+    public function professionals()
+    {
+        return $this->belongsToMany(Professional::class, 'professional_specialties', 'specialty_id', 'professional_id');
+    }
 }
