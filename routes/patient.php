4199de0f85c71166fb0986836763610d2c8c111e
jonas-amilton <jonas96@edu.unisinos.br>
2025-10-19 18:39:32 -0300

    feat: rotas para guard patient

--- routes/patient.php
+++ routes/patient.php
@@ -6,19 +6,19 @@ use App\Http\Controllers\Patient\Auth\RegisterController;
 use App\Http\Controllers\Patient\DashboardController;
 use App\Http\Controllers\Patient\AppointmentController;
 
-// Route::prefix('paciente')->name('patient.')->group(function () {
+Route::prefix('paciente')->name('patient.')->group(function () {
 
-//     Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-//     Route::post('login', [LoginController::class, 'login'])->name('login.post');
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login'])->name('login.post');
 
-//     Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-//     Route::post('register', [RegisterController::class, 'register'])->name('register.post');
+    Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [RegisterController::class, 'register'])->name('register.post');
 
-//     Route::middleware('auth.patient')->group(function () {
-//         Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::middleware('auth.patient')->group(function () {
+        Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-//         Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-//         Route::resource('appointments', AppointmentController::class);
-//     });
-// });
+        Route::resource('appointments', AppointmentController::class);
+    });
+});
