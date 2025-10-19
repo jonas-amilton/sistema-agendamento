e8d9ac10f4f705ed003d56f12a257e1b0bd43683
jonas-amilton <jonas96@edu.unisinos.br>
2025-10-19 18:38:57 -0300

    refactor: removido rotas de componentes livewire

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,7 @@
 <?php
 
-use App\Livewire\AppointmentBooking;
 use Illuminate\Support\Facades\Route;
-use App\Livewire\{
-    PatientLogin,
-    PatientRegister,
-    PatientDashboard,
-    PatientAppointment
-};
 
 Route::get('/', function () {
-    return redirect('agendar');
-});
-
-Route::get('/agendar', AppointmentBooking::class)->name('appointment.booking');
-
-
-Route::get('/login', PatientLogin::class)->name('patient.login');
-Route::get('/register', PatientRegister::class)->name('patient.register');
-
-Route::middleware('auth.patient')->group(function () {
-    Route::get('/dashboard', PatientDashboard::class)->name('patient.dashboard');
-    Route::get('/agendar', PatientAppointment::class)->name('patient.appointment');
+    return redirect('paciente/login');
 });
